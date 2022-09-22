7cfb61e4828c13025c4186355deb975bf7e02c12
LeomarSilva3 <bleomar29@gmail.com>
2022-09-22 08:52:39 -0400

    atualizando estrutura projeto

--- src/main/java/support/Assistant.java
+++ src/main/java/support/Assistant.java
@@ -11,7 +11,7 @@ public class Assistant {
 
     public static String getConfigPropertiesValues(NomeProjetoEnum projectName, String variable){
         try {
-            FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/" + projectName.getProjetoName() + "/config.properties");
+            FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/" + projectName.getProjetoName() + "/config.properties");
             properties.load(file);
         }catch (IOException e){
             e.printStackTrace();
